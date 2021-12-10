2a31ffbb101aee0e5027c72ed877b48b43c4a0ba
Antony  John <antonyj@thomsuninfocare.com>
2021-12-10 10:53:28 +0530

    AssetType Insertion

---
+++ Chef.HRMS.Services/Asset/Service/AssetTypeService.cs
@@ -0,0 +1,51 @@
+﻿using Chef.Common.Services;
+using Chef.HRMS.Models.Asset;
+using Chef.HRMS.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Services
+{
+    public class AssetTypeService : AsyncService, IAssetTypeService
+    {
+        private readonly IAssetTypeRepository assetTypeRepository;
+
+        public AssetTypeService(IAssetTypeRepository assetTypeRepository)
+        {
+            this.assetTypeRepository = assetTypeRepository;
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AssetType>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AssetType> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> InsertAsync(IEnumerable<AssetType> assetType)
+        {
+            return await assetTypeRepository.InsertAsync(assetType);
+        }
+
+        public async Task<AssetType> InsertAsync(AssetType assetType)
+        {
+            return await assetTypeRepository.InsertAsync(assetType);
+        }
+
+        public Task<int> UpdateAsync(AssetType obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
