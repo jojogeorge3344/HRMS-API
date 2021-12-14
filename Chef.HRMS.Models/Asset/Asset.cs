268ea8f3138f86346319aec032ff5bd1aa8a555f
Reshma S <reshmas@thomsuninfocare.com>
2021-12-14 18:00:06 +0530

    Change in Asset metadata repository

--- Chef.HRMS.Models/Asset/Asset.cs
+++ Chef.HRMS.Models/Asset/Asset.cs
@@ -19,15 +19,20 @@ namespace Chef.HRMS.Models
         [ForeignKey("AssetTypeMetadataId")]
         public int AssetTypeMetadataId { get; set; }
 
+
         [Required]
         public string AssetName { get; set; }
 
+        [Required]
         public DateTime Date { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public string Status { get; set; }
 
+        [Required]
         public bool IsActive { get; set; }
 
     }
