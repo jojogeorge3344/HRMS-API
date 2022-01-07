1adb45232971edf38c3a440a7c13013cee3255e0
Reshma S <reshmas@thomsuninfocare.com>
2022-01-07 11:32:40 +0530

    Assetchangetype enum and model

---
+++ Chef.HRMS.Types/AssetChangeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Types
+{
+    public enum AssetChangeType
+    {
+        Upgrade =1,
+        Defective,
+        NotWorking
+    }
+}
