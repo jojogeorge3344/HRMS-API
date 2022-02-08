516b9f5cccfcb609958947048d7400c27bf6457a
sreevinesh <77952290+sreevinesh@users.noreply.github.com>
2022-02-08 16:32:41 +0530

    Bug fixing - leave request create

--- Chef.HRMS.Web/ClientApp/src/app/features/employee/employee-wps/employee-wps-view/employee-wps-view.component.ts
+++ Chef.HRMS.Web/ClientApp/src/app/features/employee/employee-wps/employee-wps-view/employee-wps-view.component.ts
@@ -38,13 +38,31 @@ export class EmployeeWpsViewComponent implements OnInit {
     this.route.params.subscribe((params: any) => {
       this.id = parseInt(params.id, 10);
     });
-    this.getWPSGrouplist();
+   
     this.getWPSUserlistById();
   }
 
   getWPSGrouplist() {
     this.employeeWpsService.getAll().subscribe(result => {
       this.groupId = result;
+      let wpsid1 = this.wpsUserDetails[0].groupId
+      console.log("idd",wpsid1);
+      let groupName =_.find(this.groupId,["id",wpsid1]).id
+      console.log("groupname",groupName);
+      this.addForm.patchValue({groupId:groupName});
+      
+      
+     
+     //
+  //    let wpsid1 = this.wpsUserDetails[0].groupId
+  //    console.log("wpsid1",wpsid1);
+  //    console.log("group1",this.groups);
+  //    let groupNamePatch = _.find(this.groups,["id",wpsid1]).id
+  //  console.log("newwwwwwwww1", groupNamePatch);
+  //  this.addForm.patchValue({groupId:groupNamePatch});
+     //
+      console.log("groupid",this.groupId);
+
     },
       error => {
         console.error(error);
@@ -55,10 +73,10 @@ export class EmployeeWpsViewComponent implements OnInit {
   getWPSUserlistById() {
     this.employeeWpsUserService.get(this.id).subscribe(result => {
       this.wpsUserDetails = result;
-      this.addForm.patchValue({wpsId:_.find(this.wpsUserDetails,['wpsId',this.wpsUserDetails[0].wpsId])})
       console.log("wps333",this.wpsUserDetails);
       
-      this.addForm.patchValue(result);
+      this.addForm.patchValue({wpsId:this.wpsUserDetails[0].wpsId});
+      this.getWPSGrouplist();
     },
       error => {
         console.error(error);
