0ec5ef66a0029172beb21863dec2af7150ad6d64
Niranjan <niranjan.k@thomsuninfocare.com>
2023-01-02 10:31:28 +0530

    Update AssetTypeRepository.cs
    AssetType changes done

--- Chef.HRMS.Repositories/Asset/Repository/AssetTypeRepository.cs
+++ Chef.HRMS.Repositories/Asset/Repository/AssetTypeRepository.cs
@@ -26,7 +26,7 @@ namespace Chef.HRMS.Repositories
         public async Task<IEnumerable<AssetType>> GetAllAssetTypeList()
         {
 
-            var sql = @"select id,assettypename,description from hrms.assettype order by assettypename asc";
+            var sql = @"select id,assettypename,description from hrms.assettype where isarchived = false order by assettypename asc";  // Added for "where isarchived = false" by Nir
 
             return await Connection.QueryAsync<AssetType>(sql);
         }
