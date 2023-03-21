e14359a89ce6915aafd23b9c464cc5098072407d
jojo george <jojo.g@thomsuninfocare.com>
2023-03-22 00:36:04 +0530

    Visa Designation API
    API for insert, update and get data from table.

---
+++ Chef.HRMS.Models/VisaDesignation/VisaDesignation.cs
@@ -0,0 +1,22 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Models
+{
+    public class VisaDesignation : Model
+    {
+        [Required]
+        [StringLength(128)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string Name { get; set; }
+    }
+}
