7dca9df471f2e542fd396aac1f5ff4900ca1c585
Sofiya98 <125863086+Sofiya98@users.noreply.github.com>
2023-03-23 09:31:48 +0530

    get category and visa designation

--- Chef.HRMS.Models/GroupCategory/GroupCategory.cs
+++ Chef.HRMS.Models/GroupCategory/GroupCategory.cs
@@ -11,7 +11,7 @@ namespace Chef.HRMS.Models
     {
         public string Name { get; set; }
         public string Code { get; set; }
-        public bool Status { get; set; }
+        public string Status { get; set; }
 
     }
 }
