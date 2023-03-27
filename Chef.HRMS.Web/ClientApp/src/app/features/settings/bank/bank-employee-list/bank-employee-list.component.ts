7f05738ffd8919f9c477de57ea5944106300fe29
Venugopal K <venugopal.k@thomsuninfocare.com>
2023-03-27 22:53:02 +0530

    bank-master-list

--- Chef.HRMS.Web/ClientApp/src/app/features/settings/bank/bank-employee-list/bank-employee-list.component.ts
+++ Chef.HRMS.Web/ClientApp/src/app/features/settings/bank/bank-employee-list/bank-employee-list.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import{ReligionService} from '../religion-detail.service';
+import { ReligionGroup } from '../religion.model';
+import { ToasterDisplayService } from 'src/app/core/services/toaster-service.service';
+import { ConfirmModalComponent } from '@shared/dialogs/confirm-modal/confirm-modal.component';
+import { BankEmployeeCreateComponent } from '../bank-employee-create/bank-employee-create.component';
+import { BankEmployeeEditComponent } from '../bank-employee-edit/bank-employee-edit.component';
+import { BankEmployeeViewComponent } from '../bank-employee-view/bank-employee-view.component';
 
 @Component({
   selector: 'hrms-bank-employee-list',
@@ -7,9 +15,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BankEmployeeListComponent implements OnInit {
 
-  constructor() { }
+  religionDetails: ReligionGroup[] = [];
+  Codes: string[];
+  Names: string[];
+
+  constructor(
+    public modalService: NgbModal,
+    private religionService:ReligionService,
+    private toastr: ToasterDisplayService,
+  ) { }
 
   ngOnInit(): void {
+    this.getReligionlist()
   }
 
+  getReligionlist() {
+    this.religionService.getAll().subscribe(result => {
+      this.religionDetails = result;
+      this.religionDetails=this.religionDetails.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
+      this.Codes = this.religionDetails.map(a => a.code.toLowerCase());
+      this.Names = this.religionDetails.map(a => a.name.toLowerCase());
+      
+
+    },
+    error => {
+      console.error(error);
+      this.toastr.showErrorMessage('Unable to fetch the Religion List Details');
+    });
+  }
+  openCreate() {
+    const modalRef = this.modalService.open(BankEmployeeCreateComponent,
+      {size: 'lg', centered: true, backdrop: 'static' });
+    modalRef.componentInstance.code = this.Codes;
+    modalRef.componentInstance.name= this.Names;
+    modalRef.result.then((result) => {
+        if (result == 'submit') {
+          this.getReligionlist()
+        }
+    });  
+  }
+  openEdit(relDetails: ReligionGroup) {
+    const modalRef = this.modalService.open(BankEmployeeEditComponent,
+      { size: 'lg', centered: true, backdrop: 'static' });
+    modalRef.componentInstance.relDetails= relDetails;
+    modalRef.componentInstance.code = this.Codes;
+    modalRef.componentInstance.name = this.Names;
+
+    modalRef.result.then((result) => {
+      if (result == 'submit') {
+        this.getReligionlist()
+      }
+    });
+  }
+  openView(relDetails: ReligionGroup) {
+    const modalRef = this.modalService.open(BankEmployeeViewComponent,
+      { size: 'lg',centered: true, backdrop: 'static' });
+
+    modalRef.componentInstance.relDetails = relDetails;
+    modalRef.componentInstance.code = this.Codes;
+    modalRef.componentInstance.name = this.Names;
+
+    modalRef.result.then((result) => {
+      if (result == 'submit') {
+        this.getReligionlist();
+      }
+    });
+  }
+
+delete(relDetails: ReligionGroup) {
+  const modalRef = this.modalService.open(ConfirmModalComponent,
+    { centered: true, backdrop: 'static' });
+  modalRef.componentInstance.confirmationMessage = `Are you sure you want to delete the Religion ${relDetails.name}`;
+  modalRef.result.then((userResponse) => {
+    if (userResponse == true) {
+      this.religionService.delete(relDetails.id).subscribe(() => {
+        this.toastr.showSuccessMessage('Religion deleted successfully!');
+        this.getReligionlist()
+      });
+    }
+  });
 }
+}
+
+
+
