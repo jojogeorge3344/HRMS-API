0ec8d4db3d1ad5b8cbb810223bea77c3ab7c50eb
Remi S <remis@thomsuninfocare.com>
2023-03-31 02:13:51 +0530

    code cleanup

--- Chef.HRMS.Web/ClientApp/src/app/shared/employee-document-section/employee-educational-documents/employee-educational-documents.service.ts
+++ Chef.HRMS.Web/ClientApp/src/app/shared/employee-document-section/employee-educational-documents/employee-educational-documents.service.ts
@@ -1,31 +1,50 @@
-import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { EmployeeEducationalDocument } from "./employee-educational-document.model";
-import { map } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class EmployeeEducationalDocumentsService {
-
   public baseUrl: string;
   public http: HttpClient;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  constructor(http: HttpClient, @Inject("BASE_URL") baseUrl: string) {
     this.http = http;
-    this.baseUrl = baseUrl + "+/profile/educationdocument/";
+    this.baseUrl = baseUrl + "api/profile/educationdocument/";
   }
 
   get(id) {
-    return this.http.get<EmployeeEducationalDocument>(this.baseUrl + 'get/' + id).pipe(map(response => { return response; }));
+    return this.http
+      .get<EmployeeEducationalDocument>(this.baseUrl + "get/" + id)
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      );
   }
-  
+
   add(educationDocument: EmployeeEducationalDocument) {
-    return this.http.post<EmployeeEducationalDocument>(this.baseUrl + 'insert', educationDocument).pipe(map(response => { return response; }));
+    return this.http
+      .post<EmployeeEducationalDocument>(
+        this.baseUrl + "insert",
+        educationDocument
+      )
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      );
   }
 
   delete(id: number) {
-    return this.http.delete<EmployeeEducationalDocument>(this.baseUrl + 'delete/' + id).pipe(map(response => { return response; }));
+    return this.http
+      .delete<EmployeeEducationalDocument>(this.baseUrl + "delete/" + id)
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      );
   }
-
 }
