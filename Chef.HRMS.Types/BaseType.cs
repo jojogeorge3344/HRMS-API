28aee673cb72c025bf8895605ce5870477b7de92
jojo george <jojo.g@thomsuninfocare.com>
2023-03-31 16:26:13 +0530

    Leave component enums

---
+++ Chef.HRMS.Types/BaseType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Types
+{
+    public enum BaseType
+    {
+        Days = 1,
+        LifeTime = 2
+    }
+}
