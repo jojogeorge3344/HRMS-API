385309510a947fb926965d62f55a2de28552da17
Venugopal K <venugopal.k@thomsuninfocare.com>
2023-04-02 10:17:29 +0530

    edit part

--- Chef.HRMS.Web/ClientApp/src/app/features/settings/eos/eos-edit/eos-edit.component.ts
+++ Chef.HRMS.Web/ClientApp/src/app/features/settings/eos/eos-edit/eos-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToasterDisplayService } from 'src/app/core/services/toaster-service.service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { EosService } from '../eos.service';
+import { EosGroup } from '../eos.model';
 
 @Component({
   selector: 'hrms-eos-edit',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EosEditComponent implements OnInit {
 
-  constructor() { }
+  addForm: FormGroup;
+  @Input() relDetails: EosGroup;
+  @Input() Code: string[];
+  @Input() Name: string[];
+  codeExistCheck:boolean=false
+
+  constructor(
+    private eosService:EosService,
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder,
+    private toastr: ToasterDisplayService) {
+  }
 
   ngOnInit(): void {
+    this.addForm = this.createFormGroup();
+    this.addForm.patchValue(this.relDetails);
+
+  }
+
+  onSubmit() {
+    this.addForm.value.id=this.relDetails.id
+  
+    const eosForm = this.addForm.value;
+  
+    this.eosService.update(eosForm).subscribe(result => {
+      this.toastr.showSuccessMessage('The Eos updated successfully!');
+      this.activeModal.close('submit');
+    },
+      error => {
+        console.error(error);
+        this.toastr.showErrorMessage('Unable to add the Eos');
+      });
+    
+  }
+
+
+  
+  createFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      benefitName: ['', [
+        Validators.required
+      ]],
+      retrospectiveAccrual: ['', [
+        Validators.required
+      ]],
+      includeLopDays: ['', [
+        Validators.required
+      ]],
+      includeProbationDays: ['', [
+        Validators.required
+      ]],
+      eosSettlement: ['', [
+        Validators.required
+      ]],
+      includedBenefits: ['', [
+        Validators.required
+      ]],
+      
+    });
   }
 
 }
