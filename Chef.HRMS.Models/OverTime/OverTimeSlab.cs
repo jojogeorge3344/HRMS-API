3ec5e0df7946eadb3dd7cfb959cf72567ac62e3e
jojo george <jojo.g@thomsuninfocare.com>
2023-04-12 23:11:18 +0530

    overtime policy  slab model created and payrollcomponent types sorted by id

---
+++ Chef.HRMS.Models/OverTime/OverTimeSlab.cs
@@ -0,0 +1,18 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Models
+{
+    public class OverTimeSlab : Model
+    {
+        public int LowerLimit { get; set; }
+        public int UpperLimit { get; set; }
+        public int ValueVariable { get; set; }
+        public string ValueType { get; set; }
+        public int OverTimePolicyId { get; set; }
+    }
+}
