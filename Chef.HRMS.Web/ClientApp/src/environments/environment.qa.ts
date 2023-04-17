06b174dd07a04d8f21f33d5a88f8f96b9fc72fcd
Niranjan <niranjan.k@thomsuninfocare.com>
2023-04-17 12:43:38 +0530

    added enivornment .qa for qa build

---
+++ Chef.HRMS.Web/ClientApp/src/environments/environment.qa.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+  
+    app_url:'https://192.168.100.54:3015/',
+    authApiUrl:'https://192.168.100.54:3015/api/Auth/',
+    baseApiUrl: 'https://192.168.100.54:3015/api/admin/'
+  };
