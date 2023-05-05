25733002359316b55217cb8db8b742def2413fad
Niranjan <niranjan.k@thomsuninfocare.com>
2023-05-05 11:16:28 +0530

    QA and Demo URL details
    QA and Demo URL details

--- Chef.HRMS.Web/ClientApp/src/environments/environment.qa.ts
+++ Chef.HRMS.Web/ClientApp/src/environments/environment.qa.ts
@@ -1,7 +1,14 @@
 export const environment = {
     production: false,
-  
-    app_url:'https://192.168.100.54:3015/',
-    authApiUrl:'https://192.168.100.54:3015/api/Auth/',
-    baseApiUrl: 'https://192.168.100.54:3015/api/admin/'
+  //  FOR QA PORTAL
+    app_url:'https://192.168.100.54:3011/',
+    authApiUrl:'https://192.168.100.54:3011/api/Auth/',
+    baseApiUrl: 'https://192.168.100.54:3011/api/admin/',
+    reportBaseUrl: "https://192.168.100.54:3011"
+
+    //  FOR DEMO PORTAL
+    //  app_url:'https://192.168.100.54:3015/',
+    //  authApiUrl:'https://192.168.100.54:3015/api/Auth/',
+    //  baseApiUrl: 'https://192.168.100.54:3015/api/admin/',
+    //  reportBaseUrl: "https://192.168.100.54:3015"
   };
