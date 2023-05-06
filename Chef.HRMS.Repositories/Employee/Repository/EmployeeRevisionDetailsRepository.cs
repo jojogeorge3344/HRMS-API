b5a473a77caeb48638554b3ad7025fa4ed89293e
jojo george <jojo.g@thomsuninfocare.com>
2023-05-06 11:32:17 +0530

    API for employee revision salary details list by structureid and employeeid

--- Chef.HRMS.Repositories/Employee/Repository/EmployeeRevisionDetailsRepository.cs
+++ Chef.HRMS.Repositories/Employee/Repository/EmployeeRevisionDetailsRepository.cs
@@ -14,16 +14,22 @@ namespace Chef.HRMS.Repositories
         {
         }
 
-        public async Task<IEnumerable<EmployeeRevisionSalaryView>> GetEmployeeRevisionSalaryDetails(int payrollStructureId)
+        public async Task<IEnumerable<EmployeeRevisionSalaryView>> GetEmployeeRevisionSalaryDetails(int payrollStructureId,int employee)
         {
-            var sql = @"SELECT DISTINCT pcc.payrollcomponentid ,pcc.shortcode,pcc.name,pcc.maximumlimit,pc.formula
+            var sql = @"SELECT DISTINCT pcc.payrollcomponentid ,pcc.shortcode,pcc.name,pc.formula,escd.monthlyamount
                         FROM hrms.payrollcomponentconfiguration pcc
                         INNER JOIN hrms.payrollcalculation pc
                         ON pcc.payrollstructureid = pc.payrollstructureid
+                        AND pcc.payrollcomponentid = pc.payrollcomponentid
+                        INNER JOIN hrms.employeesalaryconfigurationdetails escd
+                        ON escd.payrollstructureid = pcc.payrollstructureid
+                        AND escd.payrollcomponentid = pcc.payrollcomponentid
                         WHERE pcc.payrollstructureid = @payrollStructureId
-                        AND pcc.isarchived = false";
+                        AND escd.employeeid = @employee
+                        AND escd.isarchived = false
+                        ORDER BY pcc.shortcode ASC";
 
-            return await Connection.QueryAsync<EmployeeRevisionSalaryView>(sql, new { payrollStructureId });
+            return await Connection.QueryAsync<EmployeeRevisionSalaryView>(sql, new { payrollStructureId, employee });
         }
     }
 }
