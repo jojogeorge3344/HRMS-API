361ffe25370530308c233cf60f0496ef9f9b1dac
Niranjan <niranjan.k@thomsuninfocare.com>
2023-05-07 19:41:20 +0530

    LeaveAccruals changes
    LeaveAccruals changes

---
+++ Chef.HRMS.Models/PayrollProcessing/LeaveAccrualfinDetails.cs
@@ -0,0 +1,22 @@
+﻿using Chef.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Models.PayrollProcessing
+{
+    public class LeaveAccrualfinDetails : ViewModel
+    {
+        public int Id { get; set; }
+
+        public int LeaveAccrualDetailsId { get; set; }
+        
+        public decimal LeaveAccrualAmount { get; set; }
+
+        public int DrAccount { get; set; }
+
+        public int CrAccount { get; set; }
+    }
+}
