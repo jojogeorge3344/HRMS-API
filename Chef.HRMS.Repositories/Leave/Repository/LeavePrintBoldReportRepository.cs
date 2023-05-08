d4f4222a01c6b9ba2afdf92ee6f78e8da1d7ac66
Manju M <manju.m@thomsuninfocare.com>
2023-05-08 13:39:34 +0530

    Update LeavePrintBoldReportRepository.cs

--- Chef.HRMS.Repositories/Leave/Repository/LeavePrintBoldReportRepository.cs
+++ Chef.HRMS.Repositories/Leave/Repository/LeavePrintBoldReportRepository.cs
@@ -10,14 +10,15 @@ namespace Chef.HRMS.Repositories
     {
         public async Task<IEnumerable<LeaveRequestPrintBoldReport>> GetLeaveRequestDetailsAsync(int id)
         {
-            string query = string.Format(@"SELECT 
-	                                            (Concat(e.firstname, ' ', e.lastname)) as EmployeeName,l.fromdate,l.todate,
+            string query = string.Format(@"SELECT (Concat(e.firstname, ' ', e.lastname)) as EmployeeName,l.fromdate,l.todate,
 	                                            (Concat(e.firstname, ' ', e.lastname)) as RequestedBy, 
-	                                            l.createddate as RequestedOn,lc.description AS LeaveType,lc.description AS LeaveName 
-	                                            l.numberofdays AS NumberOfDays ,l.rejoindate,l.description
-                                            FROM hrms.leave l
+	                                            l.createddate as RequestedOn,lc.description AS LeaveType,lc.description AS LeaveName, 
+	                                            l.numberofdays AS NumberOfDays ,l.rejoindate,l.description,e.firstname AS ApprovedBy,
+                                                e.firstname AS PreparedBy
+                                                FROM hrms.leave l
                                             LEFT JOIN hrms.HRMSEmployee e ON l.employeeid = e.id 
                                             LEFT JOIN hrms.leavecomponent lc ON  l.leavecomponentid=lc.id
+                                            LEFT JOIN hrms.leave hl ON hl.approvedby=e.id 
                                              WHERE l.id={0}", id);
             var result = await DatabaseSession.QueryAsync<LeaveRequestPrintBoldReport>(query);
             return result;
