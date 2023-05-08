502a062781fc5eb95a5161453c83ea8670af0d8f
Manju M <manju.m@thomsuninfocare.com>
2023-05-08 13:16:44 +0530

    Updated

--- Chef.HRMS.Repositories/Leave/Repository/LeavePrintBoldReportRepository.cs
+++ Chef.HRMS.Repositories/Leave/Repository/LeavePrintBoldReportRepository.cs
@@ -13,12 +13,12 @@ namespace Chef.HRMS.Repositories
             string query = string.Format(@"SELECT 
 	                                            (Concat(e.firstname, ' ', e.lastname)) as EmployeeName,l.fromdate,l.todate,
 	                                            (Concat(e.firstname, ' ', e.lastname)) as RequestedBy, 
-	                                            l.createddate as RequestedOn,lc.description AS LeaveType,
-	                                            l.numberofdays,l.rejoindate,l.description
+	                                            l.createddate as RequestedOn,lc.description AS LeaveType,lc.description AS LeaveName 
+	                                            l.numberofdays AS NumberOfDays ,l.rejoindate,l.description
                                             FROM hrms.leave l
                                             LEFT JOIN hrms.HRMSEmployee e ON l.employeeid = e.id 
                                             LEFT JOIN hrms.leavecomponent lc ON  l.leavecomponentid=lc.id
-                                            WHERE l.id={0}", id);
+                                             WHERE l.id={0}", id);
             var result = await DatabaseSession.QueryAsync<LeaveRequestPrintBoldReport>(query);
             return result;
         }
