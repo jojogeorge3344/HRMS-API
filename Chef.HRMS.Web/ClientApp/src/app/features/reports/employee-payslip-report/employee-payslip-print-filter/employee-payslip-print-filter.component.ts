456e4286f4f81b3f93fa3fbc5e94a399a184b5e3
Sofiya98 <125863086+Sofiya98@users.noreply.github.com>
2023-05-12 18:38:40 +0530

    filter

--- Chef.HRMS.Web/ClientApp/src/app/features/reports/employee-payslip-report/employee-payslip-print-filter/employee-payslip-print-filter.component.ts
+++ Chef.HRMS.Web/ClientApp/src/app/features/reports/employee-payslip-report/employee-payslip-print-filter/employee-payslip-print-filter.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeJobTitleService } from '@settings/employee/employee-job-title/employee-job-title.service';
+import { PayGroupService } from '@settings/payroll/pay-group/pay-group.service';
+import { result } from 'lodash';
 
 @Component({
   selector: 'hrms-employee-payslip-print-filter',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-payslip-print-filter.component.scss']
 })
 export class EmployeePayslipPrintFilterComponent implements OnInit {
-
-  constructor() { }
+ payGroupDetails;
+ designation;
+  constructor(
+  private payGroupService:PayGroupService,
+  private employeeJobTitleService:EmployeeJobTitleService
+  ) { }
 
   ngOnInit(): void {
+    this.getPayGroup()
   }
 
+  getPayGroup(){
+    this.payGroupService.getAll().subscribe(result=>{
+    this.payGroupDetails=result;
+    })
+  }
+  getDesignation(){
+this.employeeJobTitleService.getAll().subscribe(result=>{
+  this.designation=result;
+})
+  }
 }
