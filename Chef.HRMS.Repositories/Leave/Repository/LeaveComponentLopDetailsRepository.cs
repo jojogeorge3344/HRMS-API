dc7c844f8e2d749b7ed78a536309361192b20869
Shalu-tic <shalu.p@thomsuninfocare.com>
2023-06-02 10:00:22 +0530

    New model

---
+++ Chef.HRMS.Repositories/Leave/Repository/LeaveComponentLopDetailsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Repositories
+{
+	public class LeaveComponentLopDetailsRepository:GenericRepository<LeaveComponentLopDetails>, ILeaveComponentLopDetailsRepository
+	{
+		public LeaveComponentLopDetailsRepository(IHttpContextAccessor httpContextAccessor, ITenantConnectionFactory session) : base(httpContextAccessor, session)
+		{
+		}
+	}
+}
