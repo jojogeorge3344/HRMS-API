2e2bcf27cd7c035541a25e6f992b7993c7686ffd
jojo george <jojo.g@thomsuninfocare.com>
2023-06-05 20:02:22 +0530

    API changes and Report Design for employee directory report

---
+++ Chef.HRMS.Repositories/Report/Interface/IEmployeeDirectoryReportRepository.cs
@@ -0,0 +1,20 @@
+﻿using Chef.HRMS.Models.Report;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Repositories.Report
+{
+    public interface IEmployeeDirectoryReportRepository : IGenericRepository<EmployeeBasicDetailsReport>
+    {
+        Task<EmployeeBasicDetailsReport> GetBasicDetailsByEmployeeId(int employeeId);
+        Task<JobFillingReportView> GetJobFillingDetailsByEmployeeId(int employeeId);
+        Task<WPSDetailsReportView> GetWPSDetailsByemployeeId(int employeeId);
+        Task<AddressDetailsReportView> GetAddressDetailsByEmployeeId(int employeeId);
+        Task<IEnumerable<SalaryDetailsReportView>> GetSalaryDetailsByEmployeeId(int employeeId);
+        Task<JobDetailsReportView> GetByEmployeeId(int employeeId);
+
+    }
+}
