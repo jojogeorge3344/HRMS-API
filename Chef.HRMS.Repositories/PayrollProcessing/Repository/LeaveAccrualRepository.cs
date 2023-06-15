9c8750ae3129e98d9af9d6af50761aea88280cb4
AnjuRaman <anju.r@thomsuninfocare.com>
2023-06-16 00:28:11 +0530

    View and Print Accrual Service

--- Chef.HRMS.Repositories/PayrollProcessing/Repository/LeaveAccrualRepository.cs
+++ Chef.HRMS.Repositories/PayrollProcessing/Repository/LeaveAccrualRepository.cs
@@ -1,4 +1,5 @@
-﻿using Chef.HRMS.Models.PayrollProcessing;
+﻿using Chef.Common.Models;
+using Chef.HRMS.Models.PayrollProcessing;
 using Dapper;
 using System;
 using System.Collections.Generic;
@@ -20,5 +21,35 @@ namespace Chef.HRMS.Repositories.PayrollProcessing.Repository
             var sql = @"select * from hrms.leaveaccrual where leaveaccrual.accrualstatus = 1 and accrualdate = @accrualDate";
             return await Connection.QueryAsync<LeaveAccrual>(sql, new {accrualDate });
         }
+
+        public async Task<IEnumerable<LeaveAccrual>> GetLeaveAccrualsByPayrollProcessingId(int payrollProcessingId)
+        {
+            var sql = @" select la.accrualdays, la.accrualamount, la.accrualdate, la.employeeid, emp.displayname
+                        from hrms.leaveaccrual la
+                        left join hrms.jobfiling jf on jf.employeeid = la.employeeid
+                        left join hrms.hrmsemployee emp on emp.id = jf.employeeid
+                        where la.payrollprocessingid = @payrollProcessingId";
+            return await Connection.QueryAsync<LeaveAccrual>(sql, new { payrollProcessingId });
+        }
+
+        public async Task<IEnumerable<AccrualsPrintViewModel>> GetAccrualsByPayrollProcessingId(int payrollProcessingId)
+        {
+            var sql = @"select la.accrualdays as leaveaccrualdays, la.accrualamount as leaveaccrualamount,
+                                eosa.accrualdays as eosaccrualdays, eosa.accrualamount as eosaccrualamount,
+                                0 as ticketaccrualdays, ta.accrualamount as ticketaccrualamount,
+                                emp.id as employeeid, emp.displayname as employeename
+                                from hrms.eosaccrual eosa 
+						        left join hrms.ticketaccrual ta on eosa.employeeid = ta.employeeid
+						        left join hrms.leaveaccrual la on ta.employeeid = la.employeeid
+                                left join hrms.jobfiling jf on jf.employeeid = eosa.employeeid
+                                left join hrms.hrmsemployee emp on emp.id = jf.employeeid
+                                where eosa.payrollprocessingid = @payrollProcessingId 
+                                and ta.payrollprocessingid = @payrollProcessingId
+                                and la.payrollprocessingid = @payrollProcessingId";
+
+            return await Connection.QueryAsync<AccrualsPrintViewModel>(sql, new { payrollProcessingId });
+        }
+
+
     }
 }
