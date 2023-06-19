09eac0254fd8822f7a14b5e396c099f242da7355
jojo george <jojo.g@thomsuninfocare.com>
2023-06-19 16:57:03 +0530

    Getall API for List grid
    payroll component mapping screen getall API for list

--- Chef.HRMS.Services/Payroll/Interface/IPayslipSettingService.cs
+++ Chef.HRMS.Services/Payroll/Interface/IPayslipSettingService.cs
@@ -1,4 +1,5 @@
 ﻿using Chef.HRMS.Models;
+using Chef.HRMS.Models.Payroll;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,5 +18,6 @@ namespace Chef.HRMS.Services
         Task<PayslipSetting> GetPayslipSettingById(int id);
         Task<int> DeletePayslipSetting(int id);
         Task<bool> IsPayslipSettingCodeExist(string code);
+        Task<IEnumerable<PayslipSettingList>> GetAllPayslipSettings();
     }
 }
