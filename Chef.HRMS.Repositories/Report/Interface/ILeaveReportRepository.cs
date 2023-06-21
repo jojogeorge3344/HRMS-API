bbb15a5f9b289988969179a5684f71fed2a9158c
jojo george <jojo.g@thomsuninfocare.com>
2023-06-21 12:46:23 +0530

    Leave Report summary and details report API
    Unit test not completed.

--- Chef.HRMS.Repositories/Report/Interface/ILeaveReportRepository.cs
+++ Chef.HRMS.Repositories/Report/Interface/ILeaveReportRepository.cs
@@ -1,12 +1,14 @@
 ﻿using Chef.Common.Repositories;
 using Chef.HRMS.Models;
+using Chef.HRMS.Models.Report;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Chef.HRMS.Repositories
 {
-    public interface ILeaveReportRepository : IGenericRepository<LeaveReportView>
+    public interface ILeaveReportRepository : IGenericRepository<LeaveSummaryReportView>
     {
-        Task<IEnumerable<LeaveReportView>> GetLeaveReportDetails(int offSet);
+        Task<IEnumerable<LeaveSummaryReportView>> GetLeaveSummaryReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategory, string leaveComponentIds, string employeeIds);
+        Task<IEnumerable<LeaveDetailedReportView>> GetLeaveDetailedReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategory, string leaveComponentIds, string employeeIds);
     }
 }
