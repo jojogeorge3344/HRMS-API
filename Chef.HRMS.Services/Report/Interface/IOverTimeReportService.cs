9c5b8cc5cd82b60aca5d1c4eb9cbbb0161740661
Niranjan <niranjan.k@thomsuninfocare.com>
2023-06-23 09:35:56 +0530

    OverTime_Report

---
+++ Chef.HRMS.Services/Report/Interface/IOverTimeReportService.cs
@@ -0,0 +1,18 @@
+﻿using Chef.Common.Services;
+using Chef.HRMS.Models;
+using Chef.HRMS.Models.Report;
+using OfficeOpenXml.Table.PivotTable;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Chef.HRMS.Services
+{
+    
+    public interface IOverTimeReportService : IAsyncService<OverTimeSummaryReportView>
+    {
+        Task<IEnumerable<OverTimeSummaryReportView>> GetOverTimeSummaryReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategory, string overTimePolicyIds, string employeeIds);
+        Task<IEnumerable<OverTimeDetailedReportView>> GetOverTimeDetailedReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategory, string overTimePolicyIds, string employeeIds);
+    }
+}
