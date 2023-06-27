cec54a64cc806652b86da0484c06288acb66f16a
jojo george <jojo.g@thomsuninfocare.com>
2023-06-27 12:41:54 +0530

    FinalSettlement API

---
+++ Chef.HRMS.Repositories/FinalSettlement/Repository/FinalSettlementFindTLSRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chef.HRMS.Repositories.FinalSettlement
+{
+    public class FinalSettlementFindTLSRepository : TenantRepository<FinalSettlementFindTLS>, IFinalSettlementFindTLSRepository
+    {
+        public FinalSettlementFindTLSRepository(IHttpContextAccessor httpContextAccessor, ITenantConnectionFactory session) : base(httpContextAccessor, session)
+        {
+
+        }
+    }
+}
---
+++ Chef.HRMS.Repositories/FinalSettlement/Repository/FinalSettlementRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Chef.HRMS.Repositories.FinalSettlement
+{
+    public class FinalSettlementRepository : TenantRepository<Chef.HRMS.Models.FinalSettlement>, IFinalSettlementRepository
+    {
+        public FinalSettlementRepository(IHttpContextAccessor httpContextAccessor, ITenantConnectionFactory session) : base(httpContextAccessor, session)
+        {
+
+        }
+    }
+}
