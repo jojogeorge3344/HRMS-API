37a5170d8fed872af4d406355d5ba598d05bc822
Sofiya98 <125863086+Sofiya98@users.noreply.github.com>
2023-06-28 17:48:17 +0530

    report changes

--- Chef.HRMS.Models/Report/LeaveDetailedReportView.cs
+++ Chef.HRMS.Models/Report/LeaveDetailedReportView.cs
@@ -12,16 +12,9 @@ namespace Chef.HRMS.Models.Report
 {
     public class LeaveDetailedReportView : Model
     {
-        public int EmployeeId { get; set; }
         public string EmployeeCode { get; set; }
         public string EmployeeFullName { get; set; }
         public string LeaveComponentName { get; set; }
-        public string PaygroupName { get; set; }
-        public string DesignationName { get; set; }
-        public string LocationName { get; set; }
-        public DepartmentType Department { get; set; }
-        public string DepartmentName => EnumExtension.GetDescription(Department);
-        public string CategoryName { get; set; }
         public string ReportType { get; set; }
         public DateTime ToDate { get; set; }
         public DateTime FromDate { get; set; }
--- Chef.HRMS.Repositories/Report/Interface/ILeaveReportRepository.cs
+++ Chef.HRMS.Repositories/Report/Interface/ILeaveReportRepository.cs
@@ -8,7 +8,8 @@ namespace Chef.HRMS.Repositories
 {
     public interface ILeaveReportRepository : IGenericRepository<LeaveSummaryReportView>
     {
-        Task<IEnumerable<LeaveSummaryReportView>> GetLeaveSummaryReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategory, string leaveComponentIds, string employeeIds);
-        Task<IEnumerable<LeaveDetailedReportView>> GetLeaveDetailedReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategory, string leaveComponentIds, string employeeIds);
+        Task<IEnumerable<LeaveSummaryReportView>> GetLeaveSummaryReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategoryIds, string leaveComponentIds, string employeeIds);
+        Task<IEnumerable<LeaveDetailedReportView>> GetLeaveDetailedReportDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategoryIds, string leaveComponentIds, string employeeIds);
+        Task<IEnumerable<LeaveReportHeader>> GetLeaveSummaryReportHeaderDetails(string reportType, DateTime fromDate, DateTime toDate, string paygroupIds, string designationIds, string locationIds, string departmentIds, string employeeCategoryIds, string leaveComponentIds, string employeeIds);
     }
 }
